4f46476077c24ff3154b72dc190dec715a6ab512
Amir <amir.serbic@yahoo.com>
2025-07-31 00:10:42 +0200

    bazaasdfsdfgsdfd

--- backend/dist/utils/llm.js
+++ backend/dist/utils/llm.js
@@ -1,11 +1,15 @@
 "use strict";
+// backend/src/utils/llm.ts
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getSolution = getSolution;
+require("dotenv/config");
 const openai_1 = __importDefault(require("openai"));
-const openai = new openai_1.default();
+const openai = new openai_1.default({
+    apiKey: process.env.OPENAI_API_KEY
+});
 /**
  * Vraća tekst rješenja (HR ili EN) + CTA
  */
