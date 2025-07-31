d9825cf9a8dcdd2ae9eb4b964249de35e563b15f
Amir <amir.serbic@yahoo.com>
2025-07-31 02:02:14 +0200

    bazaasdfsdfgsdfsdfd

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   plugins: [
     react(),
@@ -15,9 +14,14 @@ export default defineConfig(({ mode }) => ({
     },
   },
   server: {
+    host: "localhost",
     port: 5173,
     proxy: {
-      "/api": "http://localhost:3000", // 👈 prosljeđuje sve /api pozive na backend
+      "/api": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+        secure: false,
+      },
     },
   },
 }));
